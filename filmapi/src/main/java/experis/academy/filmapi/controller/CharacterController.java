025a0c320032178dfd2fd2ccf2faca84c5a4cd43
98emre <emre-98@live.se>
2023-08-16 16:31:54 +0200

    feat: created character

---
+++ filmapi/src/main/java/experis/academy/filmapi/controller/CharacterController.java
@@ -0,0 +1,10 @@
+package experis.academy.filmapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/characters") // gör om
+public class CharacterController {
+
+}
