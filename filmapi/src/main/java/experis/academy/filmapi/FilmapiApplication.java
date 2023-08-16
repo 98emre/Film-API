6b298ac46a05b1de12d68a0c90938b775b47f77b
98emre <emre-98@live.se>
2023-08-16 12:05:22 +0200

    feat: sprin init created

---
+++ filmapi/src/main/java/experis/academy/filmapi/FilmapiApplication.java
@@ -0,0 +1,13 @@
+package experis.academy.filmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmapiApplication.class, args);
+	}
+
+}
---
+++ filmapi/src/test/java/experis/academy/filmapi/FilmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package experis.academy.filmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
