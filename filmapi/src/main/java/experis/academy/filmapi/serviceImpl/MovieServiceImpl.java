1a6b542ccf1d9764112b61f0ce2b7922cb0fc9f6
98emre <emre-98@live.se>
2023-08-16 16:34:54 +0200

    feat: create movie service implement

---
+++ filmapi/src/main/java/experis/academy/filmapi/serviceImpl/MovieServiceImpl.java
@@ -0,0 +1,51 @@
+package experis.academy.filmapi.serviceImpl;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import experis.academy.filmapi.entity.Movie;
+import experis.academy.filmapi.repository.MovieRepository;
+import experis.academy.filmapi.service.MovieService;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public Movie findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Movie add(Movie entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Movie update(Movie entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+}
