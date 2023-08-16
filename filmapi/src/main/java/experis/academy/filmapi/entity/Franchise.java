722de61ab3cdc784b6346e014ed22f6cf7e2bda4
98emre <emre-98@live.se>
2023-08-16 16:33:01 +0200

    feat: created entity franchise

---
+++ filmapi/src/main/java/experis/academy/filmapi/entity/Franchise.java
@@ -0,0 +1,64 @@
+package experis.academy.filmapi.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "franchise")
+public class Franchise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "franchise_id")
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 500, nullable = false)
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "movie_id")
+    private Set<Movie> movie;
+
+    public Franchise(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
