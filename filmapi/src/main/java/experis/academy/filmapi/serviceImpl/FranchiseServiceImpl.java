f14e61627b35384930a0f70aec9d6d26f1e35059
98emre <emre-98@live.se>
2023-08-16 16:35:10 +0200

    feat: created franchise service implement

---
+++ filmapi/src/main/java/experis/academy/filmapi/serviceImpl/FranchiseServiceImpl.java
@@ -0,0 +1,52 @@
+package experis.academy.filmapi.serviceImpl;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import experis.academy.filmapi.entity.Franchise;
+import experis.academy.filmapi.repository.FranchiseRepository;
+import experis.academy.filmapi.service.FranchiseService;
+
+@Service
+public class FranchiseServiceImpl implements FranchiseService {
+
+    private final FranchiseRepository franchiseRepository;
+
+    @Autowired
+    public FranchiseServiceImpl(FranchiseRepository franchiseRepository) {
+        this.franchiseRepository = franchiseRepository;
+    }
+
+    @Override
+    public Franchise findById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Collection<Franchise> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Franchise add(Franchise entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Franchise update(Franchise entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+}
