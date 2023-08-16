0a59c64cf13cd0561a2c246e4224910833a88da8
98emre <emre-98@live.se>
2023-08-16 16:36:15 +0200

    feat: created character repository

--- filmapi/src/main/java/experis/academy/filmapi/repository/CharacterRepository.java
+++ filmapi/src/main/java/experis/academy/filmapi/repository/CharacterRepository.java
@@ -3,6 +3,8 @@ package experis.academy.filmapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import experis.academy.filmapi.entity.Character;
+
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
 
