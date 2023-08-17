0058a848ba4cb8d432a6de2bce14796f6c093cd5
98emre <emre-98@live.se>
2023-08-17 14:52:40 +0200

    fix: movie controller

---
+++ filmapi/src/main/java/experis/academy/filmapi/controller/MovieController.java
@@ -0,0 +1,20 @@
+package experis.academy.filmapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import experis.academy.filmapi.service.MovieService;
+
+@RestController
+@RequestMapping(path = "api/movie")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+}
