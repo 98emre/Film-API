8e572211d66f1a61aadb84bd409b2d9ce02238fd
98emre <73110985+98emre@users.noreply.github.com>
2023-08-22 16:28:51 +0200

    Update CharacterServiceImpl.java

--- filmapi/src/main/java/experis/academy/filmapi/serviceImpl/CharacterServiceImpl.java
+++ filmapi/src/main/java/experis/academy/filmapi/serviceImpl/CharacterServiceImpl.java
@@ -2,13 +2,14 @@ package experis.academy.filmapi.serviceImpl;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import experis.academy.filmapi.model.MovieCharacter;
+import experis.academy.filmapi.mapper.CharacterMapper;
+import experis.academy.filmapi.model.Character;
+import experis.academy.filmapi.model.dto.CharacterDto;
 import experis.academy.filmapi.repository.CharacterRepository;
 import experis.academy.filmapi.service.CharacterService;
 
@@ -16,31 +17,42 @@ import experis.academy.filmapi.service.CharacterService;
 public class CharacterServiceImpl implements CharacterService {
 
     private final CharacterRepository characterRepository;
+    private final CharacterMapper characterMapper;
 
     @Autowired
-    public CharacterServiceImpl(CharacterRepository characterRepository) {
+    public CharacterServiceImpl(CharacterRepository characterRepository, CharacterMapper characterMapper) {
         this.characterRepository = characterRepository;
+        this.characterMapper = characterMapper;
     }
 
     @Override
-    public MovieCharacter findById(Integer id) {
-        return characterRepository.findById(id).orElse(null);
+    public CharacterDto findById(Integer id) {
+        return characterRepository.findById(id).map(characterMapper::toCharacterDto).orElse(null);
     }
 
     @Override
-    public Collection<MovieCharacter> findAll() {
-        return characterRepository.findAll().stream().sorted(Comparator.comparingInt(MovieCharacter::getId))
+    public Collection<CharacterDto> findAll() {
+        return characterRepository.findAll().stream()
+                .sorted(Comparator.comparingInt(Character::getId))
+                .map(characterMapper::toCharacterDto)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public MovieCharacter add(MovieCharacter entity) {
-        return characterRepository.save(entity);
+    public CharacterDto add(CharacterDto characterDto) {
+        Character character = characterMapper.toCharacter(characterDto);
+        Character savedCharacter = characterRepository.save(character);
+        return characterMapper.toCharacterDto(savedCharacter);
     }
 
     @Override
-    public MovieCharacter update(MovieCharacter entity) {
-        return characterRepository.save(entity);
+    public CharacterDto update(CharacterDto characterDto) {
+        if (!characterRepository.existsById(characterDto.getId())) {
+            return null; // or throw an exception
+        }
+        Character character = characterMapper.toCharacter(characterDto);
+        Character updatedCharacter = characterRepository.save(character);
+        return characterMapper.toCharacterDto(updatedCharacter);
     }
 
     @Override
