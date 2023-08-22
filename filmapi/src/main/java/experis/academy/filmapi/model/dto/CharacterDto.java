d76edc124f11dbcc54059c9e93be4bccadfc9d14
98emre <emre-98@live.se>
2023-08-22 11:28:30 +0200

    feat: added character dto

---
+++ filmapi/src/main/java/experis/academy/filmapi/model/dto/CharacterDto.java
@@ -0,0 +1,20 @@
+package experis.academy.filmapi.model.dto;
+
+import java.util.Set;
+
+import experis.academy.filmapi.model.Gender;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class CharacterDto {
+    private int id;
+    private String name;
+    private String alias;
+    private Gender gender;
+    private String pictureURL;
+    private Set<Integer> movieIds;
+}
