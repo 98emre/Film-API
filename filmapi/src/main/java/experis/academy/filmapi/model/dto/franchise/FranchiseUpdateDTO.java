a5981efb038f63617354fda9843025f645a0fda1
98emre <emre-98@live.se>
2023-08-23 11:20:01 +0200

    feat: added dto for update and post and change name for movieDto to movieDTO

---
+++ filmapi/src/main/java/experis/academy/filmapi/model/dto/franchise/FranchiseUpdateDTO.java
@@ -0,0 +1,10 @@
+package experis.academy.filmapi.model.dto.franchise;
+
+import lombok.Data;
+
+@Data
+public class FranchiseUpdateDTO {
+    private int id;
+    private String name;
+    private String description;
+}
